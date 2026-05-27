2fdb5ebb18cfc14f36ae8f4999819dbb6f84add4
Eric Fluharty <fluinc@gmail.com>
2026-05-26 20:28:28 -0400

    root route

--- src/lib/routes/main.php
+++ src/lib/routes/main.php
@@ -6,7 +6,7 @@ use APIRouter\Response;
 global $router;
 global $twig;
 
-$router->get('/hello', function (ServerRequest $req) use ($twig) {
+$router->get('/', function (ServerRequest $req) use ($twig) {
     $body = $twig->render('index.twig');
     
     return new Response(200, [], $body);
